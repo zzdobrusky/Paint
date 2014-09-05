89e79845c9415fa3caecc4ff2c0b309730443f40
zbynek <zzdobrusky@gmail.com>
2014-09-04 22:31:33 -0600

    Initial commit

---
+++ app/src/main/java/edu/utah/cs4962/paint/PaintActivity.java
@@ -0,0 +1,16 @@
+package edu.utah.cs4962.paint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PaintActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint);
+    }
+
+}
